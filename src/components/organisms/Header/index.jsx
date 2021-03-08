4a8fff3b97249ee36e8ddf4e81ec59de032c1891
Francisco Gustavo <hidalgo_fco520@outlook.com>
2021-03-07 20:46:26 -0600

    improve: Header with user, feat: add logout

--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
+import { useStateValue } from '../../../store/StateProvider';
+import { setUser } from '../../../store/actions';
+import { auth } from '../../../firebase';
+import DefaultAvatar from '../../../assets/img/default-avatar.svg'
 import './styles.css';
 
 const Header = ({ title }) => {
+  const [, dispatch] = useStateValue();
+
+  const handleClickLogout = () => {
+    auth.signOut()
+      .then(() => {
+        dispatch(setUser(null));
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }
+
   return (
     <header className="header">
       <div className="header__left">
@@ -10,7 +26,15 @@ const Header = ({ title }) => {
         <h1 className="header__title">{title}</h1>  
       </div>
       <div className="header__right">
-
+        <div className="header__user">
+          <img src={DefaultAvatar} alt="default avatar" />
+          <div className="header__userMenuContainer">
+            <div className="header__userMenu">
+              <button>Perfil</button>
+              <button onClick={handleClickLogout}>Salir</button>
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
